edf196e5ffc8ca24d45af0658b44fa44b398c24a
Serene Leyba <jcleyba92@gmail.com>
2023-02-07 18:06:26 -0700

    update

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+var moment = require('moment-timezone');
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("dateformat", function(dateIn) {
+    return moment(dateIn).tz('GMT').format('DD MMM YYYY');
+});
+    eleventyConfig.setTemplateFormats([
+      "md",
+      "njk",
+      "woff2",
+      "png",
+      "js",
+      "css" // css is not yet a recognized template extension in Eleventy
+    ]);
+  };
